6275f6db54ecaa353d4cb1551e7d65378cd010d2
pozil <>
2018-08-13 19:04:44 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# cometd-node-client
-CometD client for Node.js that adds support for promises
+# CometD client for Node.js that adds support for promises
+
+## Installation
+Add the client to your project by running:<br/>
+`npm install cometd-node-promise-client`
+
+## Usage
+
+1) Instatiate the CometD client
+
+1.a) with the default console logger:
+```js
+// 
+const cometd = new CometdClient());
+```
+
+OR
+
+1.b) with the Winston logger:
+```js
+Winston.loggers.add('COMETD', {
+  console: { level: 'info', colorize: true, label: 'COMETD' }
+});
+const cometd = new CometdClient(Winston.loggers.get('COMETD'));
+```
+
+2) Configure the CometD client
+```js
+cometd.configure({
+    url: cometdServerUrl,
+    requestHeaders: { Authorization: 'Bearer ' + this.session.access_token }, // Optional parameter if you wan to pass additional headers
+    appendMessageTypeToURL: false
+});
+```
+
+3) Handshake with the CometD server and subscribe to a `topicName` topic:
+```js
+cometd.connect().then(() => {
+    // Connection succeeded
+    cometd.subscribe(topicName, onMessageReceived).then(subscription => {
+        // This promise resolves when the client has succesfully subscribed
+    });
+});
+```
+
+The `onMessageReceived` callback function is executed each time a `topicName` message is received.
+
+Alternatively you can batch subscriptions:
+```js
+const cometdPromises = [
+    cometd.subscribe(topic1, onMessage1Received),
+    cometd.subscribe(topic2, onMessage2Received)
+];
+cometd.batch(cometdPromises).then(() => {
+    // This promise resolves when the batch is processed
+});
+```
+
+4) If you wish to unsusbribe from a topic:
+```js
+cometd.unsubscribe(subscription).then(() => {
+    // This promise resolves when the client has succesfully unsubscribed
+});
+```
+
+5) When you no longer need your CometD client, disconnect from the server:
+```js
+cometd.disconnect().then(() => {
+    // This promise resolves when the client has succesfully disconnected
+});
+```
+
+Disconnect unsubscribes from all subscriptions.
---
+++ src/cometd-client.js
@@ -0,0 +1,96 @@
+// Enable WebSockets for CometD
+var window = {};
+window.WebSocket = require('ws');
+
+require('cometd-nodejs-client').adapt();
+const cometdlib = require('cometd');
+
+
+module.exports = class CometdClient {
+
+  constructor(logger = console) {
+    this.logger = logger;
+    this.cometd = new cometdlib.CometD();
+    this.subscriptions = [];
+  }
+
+  configure(config) {
+    this.cometd.configure(config);
+  }
+
+  connect() {
+    this.logger.debug('Connecting to CometD server...');
+    return new Promise((resolve, reject) => {
+      this.cometd.handshake(handshake => {
+        if (handshake.successful) {
+          this.logger.debug('Successfully connected to CometD server.');
+          resolve();
+        } else {
+          const error = 'Unable to connect to CometD ' + JSON.stringify(handshake);
+          this.logger.error(error);
+          reject(error);
+        }
+      });
+    });
+  }
+
+  subscribe(topic, onMessageCallback) {
+    this.logger.debug('Subscribing to '+ topic);
+    return new Promise((resolve, reject) => {
+      const subscription = this.cometd.subscribe(topic, onMessageCallback, null, subResponse => {
+        if (subResponse.successful) {
+          this.logger.debug('Successfully subscribed to '+ topic);
+          resolve(subscription);
+        } else {
+          const error = 'Failed to subscribe to '+ subResponse.subscription +', reason: '+ subResponse.error;
+          this.logger.error(error);
+          reject(error);
+        }
+      });
+      this.subscriptions.push(subscription);
+    });
+  }
+
+  unsubscribe(subscription) {
+    this.logger.debug('Unsubscribing from '+ subscription.channel);
+    return new Promise((resolve, reject) => {
+      this.cometd.unsubscribe(subscription, unsubResponse => {
+        if (unsubResponse.successful) {
+          this.logger.debug('Successfully unsubscribed from '+ subscription.channel);
+          resolve();
+        } else {
+          const error = 'Failed to unsubscribe from '+ subscription.channel +', reason: '+ unsubResponse.error;
+          this.logger.error(error);
+          reject(error);
+        }
+      });
+    });
+  }
+
+  batch(cometdPromises) {
+    return new Promise((resolve, reject) => {
+      this.cometd.startBatch();
+      Promise.all(cometdPromises).then(() => {
+        this.cometd.endBatch();
+        resolve();
+      }).catch(e => {
+        this.cometd.endBatch();
+        reject(e);
+      });
+    });
+  }
+
+  disconnect() {
+    return new Promise((resolve, reject) => {
+      this.logger.debug('Batch unsubscribing...');
+      const cometdPromises = this.subscriptions.map(subscription => (this.unsubscribe(subscription)) );
+      this.batch(cometdPromises).then(() => {
+        this.logger.debug('Disconnecting...');
+        this.cometd.disconnect(false, null, () => {
+          this.logger.debug('Successfully disconnected');
+          resolve();
+        });
+      });
+    });
+  }
+}
